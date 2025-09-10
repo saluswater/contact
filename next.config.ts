7182521e374d831cc9b201c031ed9fb83a0e464f
Luiz Gouveia <lcpgou@gmail.com>
2025-09-10 17:16:19 -0300

    next.config.ts updated

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/salus-new-lps',
-  assetPrefix: '/salus-new-lps',
   trailingSlash: true,
   images: {
     unoptimized: true
