ea55042a173966bd55ce48252b320f9860040eb1
Luiz Gouveia <lcpgou@gmail.com>
2025-09-10 16:57:12 -0300

    configure Github Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/contact',
+  assetPrefix: '/contact',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
